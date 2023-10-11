2e8db92624539ccc78fbd6c32853cbb12494e6fe
Raduga <19dima991999@mail.ru>
2023-10-11 18:33:56 +0300

    footer stage

--- src/components/core/footer/Footer.vue
+++ src/components/core/footer/Footer.vue
@@ -1,17 +1,10 @@
 <template>
     <footer class="footer">
         <v-row align="center" justify="center" dense>
-            <v-col cols="auto">
-                <HoverIcon :icon="'mdi-facebook'" :hoveredColor="'#3b5998'"></HoverIcon>
-            </v-col>
-            <v-col cols="auto">
-                <HoverIcon :icon="'mdi-github'" :hoveredColor="'#000'"></HoverIcon>
-            </v-col>
-            <v-col cols="auto">
-                <HoverIcon :icon="'mdi-linkedin'" :hoveredColor="'#0077B5'"></HoverIcon>
-            </v-col>
-            <v-col cols="auto">
-                <HoverIcon :localImage="'technology-icons/android.png'" :hoveredColor="'#0077B5'"></HoverIcon>
+            <v-col v-for="item in socials" cols="auto">
+                <HoverIcon @click="navigateTo" :url="item.url" :localImage="item.localImage" :icon="item.icon"
+                    :hoveredColor="item.hoveredColor">
+                </HoverIcon>
             </v-col>
         </v-row>
     </footer>
@@ -23,7 +16,39 @@ import HoverIcon from "../../shared/HoverIcon.vue";
 export default {
     name: "Footer",
     data: () => ({
+        socials: [
+            {
+                icon: "mdi-facebook",
+                hoveredColor: "#3b5998",
+                url: "https://www.facebook.com/profile.php?id=100090931236023",
+            },
+            {
+                icon: "mdi-github",
+                hoveredColor: "#000000",
+                url: "https://github.com/FursiK911",
+            },
+            {
+                icon: "mdi-linkedin",
+                hoveredColor: "#0077B5",
+                url: "https://www.linkedin.com/in/dmitry-fursov-251097213/",
+            },
+            {
+                localImage: "social-icons/vk.svg",
+                hoveredColor: "#0077ff",
+                url: "https://vk.com/dimafursov",
+            },
+            {
+                localImage: "social-icons/telegram.svg",
+                hoveredColor: "#2AABEE",
+                url: "https://t.me/FursiK911",
+            },
+        ]
     }),
+    methods: {
+        navigateTo(url) {
+            window.open(url, '_blank', 'noreferrer');
+        },
+    },
     components: {
         HoverIcon,
     }
--- src/components/shared/HoverIcon.vue
+++ src/components/shared/HoverIcon.vue
@@ -1,9 +1,10 @@
 <template>
     <v-row class="icon-container" justify="center" align="center" :style="{ backgroundColor, color, }"
-        @mouseover="changeColor" @mouseleave="resetColor" no-gutters>
+        @mouseover="changeColor" @mouseleave="resetColor" @click="onClickHandler" no-gutters>
         <v-col cols="auto">
             <v-icon v-if="icon" :icon="icon" size="36"></v-icon>
-            <v-img v-else-if="localImage" :src="getImageUrl()" height="36" width="36"></v-img>
+            <v-img v-else-if="localImage" :src="getImageUrl(this.localImage)" height="36" width="36"
+                :style="{ opacity }"></v-img>
         </v-col>
     </v-row>
 </template>
@@ -14,24 +15,31 @@ export default {
     data: () => ({
         backgroundColor: "#333",
         color: "rgba(255, 255, 255, 0.6)",
+        opacity: "0.6",
     }),
     methods: {
         changeColor() {
             this.backgroundColor = this.hoveredColor || '#333';
             this.color = "rgba(255, 255, 255, 1)";
+            this.opacity = "1";
         },
         resetColor() {
             this.backgroundColor = '#333';
             this.color = "rgba(255, 255, 255, 0.6)";
+            this.opacity = "0.6";
         },
         getImageUrl(name) {
-            return new URL(`@/assets/${localImage}`, import.meta.url).href
+            return new URL(`../../assets/${name}`, import.meta.url).href
+        },
+        onClickHandler() {
+            this.$emit('click', this.url);
         },
     },
     props: {
         icon: String,
         localImage: String,
         hoveredColor: String,
+        url: String,
     },
 };
 </script>
